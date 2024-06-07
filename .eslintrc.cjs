54d71b5b21d7e96e0353eb8543f0dc053193fde1
Kasprian <32979274+Kasprian@users.noreply.github.com>
2024-06-07 23:03:39 +0200

    CHanges of day 3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,37 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    // Customize your rules
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        vue: 'never'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue']
+      }
+    }
   }
 }
